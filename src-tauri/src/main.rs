37d8587f3171f20c0f27f9b1cb04aae9deec063c
Wronskode <julienlahozpro@gmail.com>
2023-12-23 11:50:18 +0100

    Bug fixed and some improvements

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use serde_json::{json, Value};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use serde_json::{json, Value};
 use sha2::{Digest, Sha256};
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
@@ -18,20 +18,50 @@ fn main() {
 }   
 
 #[tauri::command]
-async fn verify(folder1: String, folder2: String, secure: bool) -> Value {
+async fn verify(mut folder1: String, mut folder2: String, secure: bool) -> Value {
     let now = Instant::now();
+    let is_unix = cfg!(unix);
+    match folder1.chars().last() {
+        Some(k) => {
+            if k != '/' {
+                if is_unix {
+                    folder1.push('/');
+                }
+            }
+            else if k != '\\' {
+                if !is_unix {
+                    folder1.push('\\');
+                }
+            }
+        },
+        None => ()
+    }
+    match folder2.chars().last() {
+        Some(k) => {
+            if k != '/' {
+                if is_unix {
+                    folder2.push('/');
+                }
+            }
+            else if k != '\\' {
+                if !is_unix {
+                    folder1.push('\\');
+                }
+            }
+        },
+        None => ()
+    }
     let excluded_folders = Arc::new(Mutex::new(HashSet::new()));
-    excluded_folders
-        .lock()
-        .unwrap()
-        .insert("System Volume Information".to_string()); // Add here excluded folders like that
+    // excluded_folders
+    //     .lock()
+    //     .unwrap()
+    //     .insert("System Volume Information".to_string()); // Add here excluded folders like that
     let t1 = {
         let excluded_folders = excluded_folders.clone();
         thread::spawn(move || {
             check_folder(folder1.clone().into(), folder1.len(), secure, excluded_folders)
         })
     };
-
     let t2 = {
         let excluded_folders = excluded_folders.clone();
         thread::spawn(move || {
@@ -46,10 +76,11 @@ async fn verify(folder1: String, folder2: String, secure: bool) -> Value {
         Ok(hm) => hm,
         Err(_) => HashMap::new()
     };
+
     // TODO : Optimize this part
     let mut files_only_in_f1: Vec<&str> = vec![];
     let mut files_only_in_f2: Vec<&str> = vec![];
-    let mut diff_files: Vec<&str> = vec![];
+    let mut diff_files = HashSet::new();
     let mut all_files = HashSet::new();
     let mut files_in_f1 = vec![];
     let mut files_in_f2 = vec![];
@@ -60,7 +91,7 @@ async fn verify(folder1: String, folder2: String, secure: bool) -> Value {
         }
         else {
             if element.1 != f2.get(element.0).unwrap() {
-                diff_files.push(element.0.to_str().unwrap());
+                diff_files.insert(element.0.to_str().unwrap());
             }
         }
         all_files.insert(element.0);
@@ -69,6 +100,11 @@ async fn verify(folder1: String, folder2: String, secure: bool) -> Value {
         files_in_f2.push(element.0);
         if !f1.contains_key(element.0) {
             files_only_in_f2.push(element.0.to_str().unwrap());
+        }
+        else {
+            if element.1 != f1.get(element.0).unwrap() {
+                diff_files.insert(element.0.to_str().unwrap());
+            }
         }
         all_files.insert(element.0);
     }
@@ -81,7 +117,8 @@ async fn verify(folder1: String, folder2: String, secure: bool) -> Value {
         "time": format!("{:?}", now.elapsed()),
         "all files": all_files,
         "f1_files": files_in_f1,
-        "f2_files": files_in_f2
+        "f2_files": files_in_f2,
+        "excluded folders": *excluded_folders
     });
     return result;
 }
@@ -91,6 +128,7 @@ fn check_folder(path: PathBuf, len: usize, secure: bool, excluded_folders: Arc<M
     let folder = match fs::read_dir(&path) {
         Err(_) => {
             println!("Unable to open this directory {:?}\n(verify permissions)", path);
+            excluded_folders.lock().unwrap().insert(path.to_str().unwrap().to_string());
             return files_hashs;
         },
         Ok(folder) => folder
--- src/main.js
+++ src/main.js
@@ -13,14 +13,13 @@ async function verify() {
   console.log(json);
   let files_diff = json["different files"];
   if (files_diff.length == 0 && json["only folder1"].length == 0 && json["only folder2"].length == 0) {
-    verif.innerHTML = "All the files are identical and are present in each of the directories.";
+    verif.innerHTML = "All the files are identical and are present in each of the directories. ✅";
     return;
   }
-  if (files_diff.length == 0) {
+  if (json["only folder1"].length == json["Length of folder1"] && json["only folder2"].length == json["Length of folder2"] ) {
     verif.innerHTML = "All the files are different (perhaps one of the folders is included in the other?).";
     return;
   }
-
   let stringResult = `<center><table> <tbody> 
       <tr> 
       <th scope="col">File</th>
@@ -28,31 +27,41 @@ async function verify() {
       <th scope="col">In folder 2</th>
       <th scope="col">Equals ?</th>
     </tr>`;
-  for (const file of files_diff) {
+  let f1_files_set = new Set(json["f1_files"]);
+  let f2_files_set = new Set(json["f2_files"]);
+  let different_files = new Set(json["different files"])
+  for (const file of json["all files"].sort()) {
+    let i1 = f1_files_set.has(file);
+    let i2 = f2_files_set.has(file);
+    let i3 = different_files.has(file);
       stringResult += `<tr><th scope="row">${file}</th>`
-      if (json["f1_files"].includes(file)) {
+      if (i1) {
         stringResult += `<td><code>✅</code></td>`;
       }
       else {
         stringResult += `<td><code>❌</code></td>`;
       }
-      if (json["f2_files"].includes(file)) {
+      if (i2) {
         stringResult += `<td><code>✅</code></td>`;
       }
       else {
         stringResult += `<td><code>❌</code></td>`;
       }
-      if (json["different files"].includes(file)) {
+      if (!i3 && i1 && i2) {
+        stringResult += `<td><code>✅</code></td>`;
+      }
+      else if (i3 && i1 && i2) {
         stringResult += `<td><code>❌</code></td>`;
       }
       else {
-        stringResult += `<td><code>✅</code></td>`;
+        stringResult += `<td><code></code></td>`;
       }
       stringResult += `</tr>`;
   }
   verif.innerHTML = stringResult+`</table></center>`+"<br> time: "+json["time"]
   +"<br> size of folder 1 : " + json["Length of folder1"]
-  +"<br> size of folder 2 : " + json["Length of folder2"];
+  +"<br> size of folder 2 : " + json["Length of folder2"]
+  +"<br> excluded folders : " + json["excluded folders"];
 }
 
 window.addEventListener("DOMContentLoaded", () => {
